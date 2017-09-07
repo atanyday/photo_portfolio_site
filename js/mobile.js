57f75ff9a420e8c67045775b20a424ea21916a4b
Anna Bezrukova <annelia_55@mail.ru>
2017-09-07 16:25:10 +0700

    dropdown menu

--- js/mobile.js
+++ js/mobile.js
@@ -1,1 +1,17 @@
+var isCollapsed = true;
 
+$(document).ready(function(){
+    $("#portfolio_link").click(function(){
+
+        if (isCollapsed){
+            $(".dropdown-portfolio").css('display', 'block');
+            $(".dropdown-portfolio").animate( {height: '280px'}, 1000);
+            isCollapsed = false;
+        } else {
+            $(".dropdown-portfolio").animate( {height: '0px'}, 1000, null, function(){
+                $(".dropdown-portfolio").css('display', 'none');
+            });
+            isCollapsed = true;
+        }
+    });
+});
