a8b7f5f09974db380a3996278a3d4775f75a2760
Anna Bezrukova <annelia_55@mail.ru>
2017-09-10 12:02:01 +0700

    delete some mistakes+js experiment

--- js/index.js
+++ js/index.js
@@ -1,29 +1,96 @@
 // ------------------------------------------------------------------------------------------------ FOLLOW LOCATION HASH
+var currentUrl = null;
 function gotoPage(param) {
 
-    var url = window.location.hash;
+    var newUrl = window.location.hash;
 
-    if (url == "#plener") {
+    //// hiding old
+    //var sections = {
+    //    "#plener":plener,
+    //    "#studio":studio,
+    //    "#report": report,
+    //    "#world": world,
+    //    "#things": things
+    //};
+    //
+    //if (currentUrl == "#plener") {
+    //    $(plener).slideUp(400);
+    //} if (currentUrl == "#studio") {
+    //    $(studio).slideUp();
+    //} if (currentUrl == "#report") {
+    //    $(report).slideUp();
+    //} if (currentUrl == "#world") {
+    //    $(world).slideUp();
+    //} if (currentUrl == "#things") {
+    //    $(things).slideUp();
+    //}
+    //
+
+    // showing new
+    if (newUrl == "#plener") {
         showOnly(portfolio_container, side_bar_elements);
         showOnlySlide(plener, portfolio_elements);
-    } if (url == "#studio") {
+    } if (newUrl == "#studio") {
         showOnly(portfolio_container, side_bar_elements);
         showOnlySlide(studio, portfolio_elements);
-    } if (url == "#report") {
+    } if (newUrl == "#report") {
         showOnly(portfolio_container, side_bar_elements);
         showOnlySlide(report, portfolio_elements);
-    } if (url == "#world") {
+    } if (newUrl == "#world") {
         showOnly(portfolio_container, side_bar_elements);
         showOnlySlide(world, portfolio_elements);
-    } if (url == "#things") {
+    } if (newUrl == "#things") {
         showOnly(portfolio_container, side_bar_elements);
         showOnlySlide(things, portfolio_elements);
-
     }
+
+    //currentUrl = newUrl;
     console.log(window.location.hash);
+
+    //// hiding old
+    //var sections = {
+    //    "#plener": plener,
+    //    "#studio": studio,
+    //    "#report": report,
+    //    "#world":  world,
+    //    "#things": things
+    //};
+    ////
+    ////$( sections[currentUrl] ).slideUp(400, null, function(){
+    ////
+    ////
+    ////    // showing new
+    ////    if (newUrl == "#plener") {
+    ////        showOnly(portfolio_container, side_bar_elements);
+    ////        showOnlySlide(plener, portfolio_elements);
+    ////    } if (newUrl == "#studio") {
+    ////        showOnly(portfolio_container, side_bar_elements);
+    ////        showOnlySlide(studio, portfolio_elements);
+    ////    } if (newUrl == "#report") {
+    ////        showOnly(portfolio_container, side_bar_elements);
+    ////        showOnlySlide(report, portfolio_elements);
+    ////    } if (newUrl == "#world") {
+    ////        showOnly(portfolio_container, side_bar_elements);
+    ////        showOnlySlide(world, portfolio_elements);
+    ////    } if (newUrl == "#things") {
+    ////        showOnly(portfolio_container, side_bar_elements);
+    ////        showOnlySlide(things, portfolio_elements);
+    ////    }
+    ////
+    ////    console.log(window.location.hash);
+    ////});
+    //
+    //
+    //
+    //$( portfolio_container ).slideUp(400, null, function() {
+    //
+    //    $ ( sections[currentUrl] ).hide();
+    //    $ ( sections[newUrl] ).show();
+    //
+    //    $( portfolio_container ).slideDown();//, function() {
+    //});
+    //
 }
-
-
 // ---------------------------------------------------------------------------------------------- HIGHTLIGHT ACTIVE LINK
 $(document).ready(function(){
     $('ul li a').click(function(){
@@ -31,8 +98,6 @@ $(document).ready(function(){
         $(this).addClass("active");
     });
 });
-
-
 // ---------------------------------------------------------------------------------------------------------- CONTAINERS
 var portfolio_container = document.getElementById("portfolio_container");
 var price_container = document.getElementById("price_container");
@@ -86,7 +151,6 @@ var things = document.getElementById("things");
 
 var portfolio_elements = [plener, studio, report, world, things];
 
-
 //$("#plener_link").click(function(){
 //    showOnly(plener, portfolio_elements);
 //});
@@ -112,6 +176,4 @@ window.onhashchange = gotoPage;
 $( document ).ready(function() {
     gotoPage(window.location.hash);
 });
-
-
 // ------------------------------------------------------------------------------------------------------------- GALLERY
